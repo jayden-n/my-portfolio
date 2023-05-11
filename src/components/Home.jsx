1d6cc938c18fea8fa0cab3d9e1de731c005e2fcb
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-11 02:18:47 -0400

    fixed font family + typewriter animation + typos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
-
+//import Typewriter from "typewriter-effect";
+import { useTypewriter } from 'react-simple-typewriter';
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ['JavaScript Developer.', 'Front-End Engineer.'],
+    loop: {},
+  });
+
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
@@ -11,7 +17,21 @@ const Home = () => {
           Jayden Nguyen
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
+          I'm a
+          <span className="ml-4 text-4xl sm:text-7xl font-bold text-[#8892b0]">
+            {text}
+            {/* <Typewriter
+            options={
+              {
+                autoStart:true,
+                strings:["Full Stack Developer","Front-End Engineer","Cloud Enthusiast","Blockchain Enthusiast","Software Developer"],
+                delay:100,
+                loop:true,
+                deleteSpeed:100
+              }
+            }
+          /> */}
+          </span>
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a full-stack developer specializing in building (and occasionally
@@ -20,7 +40,7 @@ const Home = () => {
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
+            View my projects
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3 " />
             </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,14 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo" style={{ width: '50px' }} />
+        <Link to="home" smooth={true} duration={500}>
+          <img
+            className="cursor-pointer"
+            src={Logo}
+            alt="Logo"
+            style={{ width: '50px' }}
+          />
+        </Link>
       </div>
 
       {/* Menu */}
